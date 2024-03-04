d76482e62c0862623f787bb744b49f792e0cce13
Kalyani2022 <97873757+Kalyani2022@users.noreply.github.com>
2024-03-04 15:20:03 +0530

    Add files via upload

--- script2.py
+++ script2.py
@@ -12,4 +12,12 @@ uName = driver.find_element('name', "session_key")
 uName.send_keys("indrani.b2022@gmail.com")
 uPassword = driver.find_element('name', "session_password")
 uPassword.send_keys("Indrani_B@2022")
-driver.find_element(By.CSS_SELECTOR, "button.sign-in-form__submit-btn--full-width").click()
+# driver.find_element(By.CSS_SELECTOR, "button.sign-in-form__submit-btn--full-width").click()
+driver.find_element(By.CSS_SELECTOR, "button[type='submit']").click()
+
+driver.find_element(By.CSS_SELECTOR, "input[placeholder='Search']").send_keys("Indrani Bhattacharjee")
+time.sleep(10)
+
+driver.find_element(By.CSS_SELECTOR, "span[class='search-global-typeahead-hit__text t-16 t-black']").click()
+time.sleep(60)
+
